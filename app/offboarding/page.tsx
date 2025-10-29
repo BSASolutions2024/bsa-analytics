71153da34b182cc1facb0605669dd81fde875678
brandough7 <parot.jbryan@gmail.com>
2025-10-29 16:38:00 +0800

    fix 5

--- app/offboarding/page.tsx
+++ app/offboarding/page.tsx
@@ -1,13 +1,10 @@
+import OffboardingDashboard from "@/components/offboarding/offboarding-dashboard";
 import { columns, Offboarding } from "./columns";
 import { OffboardingDataTable } from "./data-table";
 
 export default async function Page() {
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/offboarding/get-data`, {
-        cache: "no-store",
-    });
-    const result = await res.json();
-    const data: Offboarding[] = result?.response ?? []
+   
     return (
-        <OffboardingDataTable columns={columns} data={data} />
+        <OffboardingDashboard></OffboardingDashboard>
     )
 }
---
+++ components/offboarding/offboarding-dashboard.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { columns, Offboarding } from "@/app/offboarding/columns";
+import { OffboardingDataTable } from "@/app/offboarding/data-table";
+import { useEffect, useState } from "react";
+
+export default function OffboardingDashboard() {
+    const [data, setData] = useState<Offboarding[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch(`/api/offboarding/get-data`, {
+                    cache: "no-store",
+                });
+                const result = await res.json();
+                setData(result.response ?? []); // set fetched data
+            } catch (err) {
+                console.error("Error fetching offboarding data:", err);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    return (
+        <div>
+            <OffboardingDataTable columns={columns} data={data} />
+        </div>
+    )
+}
