bb215c13194d422c1cb2e9f7bd4b84e20ceefd4a
brandough7 <parot.jbryan@gmail.com>
2025-10-29 15:39:24 +0800

    fix 2

--- auth.ts
+++ auth.ts
@@ -2,8 +2,15 @@ import NextAuth from "next-auth"
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [MicrosoftEntraID],
-    session: { strategy: "jwt", maxAge: 24 * 60 * 60 },
+    providers: [
+        MicrosoftEntraID({
+            clientId: process.env.AZURE_AD_CLIENT_ID!,
+            clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+            issuer: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/v2.0`,
+            authorization: { params: { scope: "openid profile email" } },
+        }),
+    ],
+    session: { strategy: "jwt", maxAge: 10 },
     trustHost: true,
     secret: process.env.AUTH_SECRET,
 })
