b4f91ac3e1ac43535e488f4420c1c9e4e7bc5c46
brandough7 <parot.jbryan@gmail.com>
2025-10-29 16:05:43 +0800

    fix

--- app/offboarding/page.tsx
+++ app/offboarding/page.tsx
@@ -1,9 +1,8 @@
-
 import { columns, Offboarding } from "./columns";
 import { OffboardingDataTable } from "./data-table";
 
 export default async function Page() {
-    const res = await fetch("http://localhost:3000/api/offboarding/get-data", {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/offboarding/get-data`, {
         cache: "no-store",
     });
     const result = await res.json();
