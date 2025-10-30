26df815610e54d3817ad45048895af7bf3394e6c
brandough7 <parot.jbryan@gmail.com>
2025-10-30 13:31:33 +0800

    fix entra

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         MicrosoftEntraID({
