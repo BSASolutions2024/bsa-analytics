f1ac121eaa099546358de803f29bd9faf6d2fa09
brandough7 <parot.jbryan@gmail.com>
2025-11-03 10:50:19 +0800

    util with time

--- lib/utils.ts
+++ lib/utils.ts
@@ -64,3 +64,13 @@ export function parseDateString(dateString: string): Date {
   const [day, month, year] = dateString.split("-").map(Number);
   return new Date(year, month - 1, day);
 }
+
+export function formatDateWithTime(date = new Date()) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day}-${hours}${minutes}`;
+}
